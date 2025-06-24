cdb12e0e0537fb45588bb8f1654ff919d58ab584
kindredzhang <kindred.zhang.life@gmail.com>
2025-06-24 10:30:25 +0800

    header

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import "@/app/globals.css";
 import type { Metadata } from "next";
 
 import Footer from "@/components/layout/Footer";
-import Header from "@/components/layout/header";
+import Header from "@/components/layout/Header";
 import MainContent from "@/components/layout/MainContent";
 import ThemeProvider from "@/components/theme/ThemeProvider";
 import ClientBackToTop from "@/components/ui/back2top/ClientBackToTop";
@@ -22,7 +22,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="zh-CN" suppressHydrationWarning>
-      <head />
+      <head>
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4903945973415386"
+          crossOrigin="anonymous">
+        </script>
+      </head>
       <body className="font-sans antialiased bg-gray-50 dark:bg-[#050505] relative w-screen overflow-x-hidden">
         <ThemeProvider>
           <div className="dot-pattern"></div>
